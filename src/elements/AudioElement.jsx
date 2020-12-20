a183298d0b00a53d554d271bc45adc73f82aefd5
mikesci <mike@mikesci.com>
2020-12-20 15:28:07 -0800

    funk your mudder, brudder

--- src/elements/AudioElement.jsx
+++ src/elements/AudioElement.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { getSrcFromAssetUrl, isAssetUrl } from "./elementUtilities.js";
 
 const AudioElement = {
